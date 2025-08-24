fdd54c7a9cf9024e22ebfad6219a61315f2ae7d0
seanl26@uw.edu <seanl26@uw.edu>
2025-08-24 10:44:49 -0600

    testing change to pagelayout

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -5,11 +5,18 @@ type LayoutProps = {
 function Layout({ children }: LayoutProps){
   return (
     <>
-      <div className="min-h-screen bg-slate-950 text-slate-200 antialiased">
+      <div className="min-h-screen bg-slate-950 text-slate-200 antialiased overflow-x-hidden">
+        <main className="px-4 py-10">
+          <div className="w-full max-w-xl mx-auto">
+            { children }
+        </div>
+        </main>
+      </div>
+      {/* <div className="min-h-screen bg-slate-950 text-slate-200 antialiased">
         <main className="flex items-center justify-center min-h-screen">
           { children }
         </main>
-      </div>
+      </div> */}
     </>
   )
 }
