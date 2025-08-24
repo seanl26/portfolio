79c76890b660ce53ace6c9e39fa60a45d00634aa
seanl26@uw.edu <seanl26@uw.edu>
2025-08-24 00:49:50 -0600

    completed navbar and links

---
+++ src/components/Footer.tsx
@@ -0,0 +1,56 @@
+import resume from "../assets/Sean_Liu_Resume_2027.pdf"
+
+function Footer() {
+  return (
+    <>
+      <hr className="border-1 border-slate mx-auto" />
+      <footer className="text-center text-xl">
+        <nav className="flex justify-center tracking-widest">
+
+          <div className="space-x-11">
+            <a 
+              href="mailto:seanl26@uw.edu" 
+              className="hover:text-blue-300">
+                Email
+            </a>
+            <a
+              href="www.linkedin.com/in/sohyliu"
+              className="hover:text-blue-300"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              LinkedIn
+            </a>
+            <a
+              href="https://github.com/seanl26"
+              className="hover:text-blue-300"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>
+            <a
+              href={resume}
+              className="hover:text-blue-300"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Résumé
+            </a>
+            <a
+              href="https://www.instagram.com/liuserpx/"
+              className="hover:text-blue-300"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Photos
+            </a>
+          </div>
+          
+        </nav>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,17 @@
 import Layout from "../components/PageLayout"
+import Footer from "../components/Footer";
 import me from "../assets/me.jpg"
 import { useState, useEffect, useMemo } from 'react'
 
 function Home() {
+
   // usememo prevents the dependency array from being rerendered
   const phrases = useMemo(() => [ 
-    "A Student 📝",
-    "Software Engineer 📀",
-    "Builder 🧰",
-    "Lifelong Learner 📚", 
-    "A Friend 👥",
+    "student 📝",
+    "software engineer 📀",
+    "photographer 📸",
+    "lifelong learner 📚", 
+    "your new friend 👥",
     ],
     []
   );
@@ -20,10 +22,10 @@ function Home() {
   const deletingSpeed = 50; // ms per character while deleting
   const pauseMs = 800; // pause at the end of a word before deleting
 
+  // logic for the typewriter animation
   useEffect(() => {
     const currPhrase = phrases[i] ?? ""; // sets phrase to "" if it's undefined or null
     let timer: ReturnType<typeof setTimeout>;
-
     if(!deleting) {
       if(count < currPhrase.length) {
         // typing forward
@@ -47,24 +49,38 @@ function Home() {
 
   return (
     <Layout>
+
       <section className="w-full max-w-xl mx-auto space-y-8">
-        <div className="flex items-center justify-center gap-8">
+
+        <div className="flex items-center justify-center space-x-8">
           <img
             src={me}
             alt="Sean Liu Picture"
-            className="h-35 w-35 rounded-full object-cover ring-2 ring-white-700"
+            className="h-35 w-35 rounded-full object-cover ring-2"
           />
           <div className="flex flex-col justify-start space-y-3">
             <h1 className="text-8xl font-semibold">Sean Liu</h1>
-            <p className="flex justify-center text-2xl text-slate font-mono h-8 items-center">
+            <p className="flex justify-center text-3xl font-mono h-8 items-center">
             <span>{(phrases[i] ?? "").slice(0, count)}</span>
             <span aria-hidden className="ml-1 animate-pulse">▍</span>
             </p>
           </div>
         </div>
-        <hr className="border-2 border-white mx-auto" />
-        <p className="text-xl"> Welcome to my portfolio 👋</p>
+        <hr className="border-1 border-slate mx-auto" />
+        <div className="flex flex-col gap-5 text-start text-2xl font-medium text-slate-300 space-y-3">
+          <p> hey there 👋 thanks for visiting!</p>
+          <p> 
+            i'm an undergraduate at the <a href="https://www.washington.edu" className="hover:text-blue-300 underline">university of washington</a> studying <a href="https://ischool.uw.edu/" className="hover:text-blue-300 underline">informatics</a>, concentrating in software development and data science! 
+          </p>
+          <p className="text-center">→ &nbsp; check out my <span className="font-bold hover:text-blue-300 underline">projects</span> &nbsp; ←</p>
+          <p>i'm especially interested in AI/ML applications and data analysis, but love to learn a bit of everything! i am also passionate about sustainibility and accessibility.</p>
+          <p>feel free to reach out through my links below, i would love to get to know you!</p>
+        </div>
+
+        <Footer />
+
       </section>
+
     </Layout>
   )
 }
