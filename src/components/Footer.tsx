fde04da6ae3059bbf618e9085ff8f18679bebb5c
seanl26@uw.edu <seanl26@uw.edu>
2025-08-27 17:48:19 -0600

    change footer width

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ function Footer() {
 
       <footer className="mx-auto w-11/12 sm:w-4/5 md:w-2/3 lg:w-1/2 xl:w-2/5 text-center text-base sm:text-lg md:text-xl lg:text-2xl mt-4">
         <nav className="flex justify-center font-bold tracking-medium">
-          <div className="flex flex-wrap justify-center gap-x-6 sm:gap-x-8 md:gap-x-10 lg:gap-x-12 gap-y-2">
+          <div className="flex justify-center gap-x-3 sm:gap-x-6">
             <a href="mailto:seanl26@uw.edu" className="hover:text-blue-300">Email</a>
             <a href="https://www.linkedin.com/in/sohyliu" className="hover:text-blue-300" target="_blank" rel="noopener noreferrer">LinkedIn</a>
             <a href="https://github.com/seanl26" className="hover:text-blue-300" target="_blank" rel="noopener noreferrer">GitHub</a>
