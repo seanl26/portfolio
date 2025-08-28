b727b27063b1520a0e7dd5ea91c30610e5062dcc
seanl26@uw.edu <seanl26@uw.edu>
2025-08-28 02:28:35 -0600

    more height changes

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -7,7 +7,7 @@ function Layout({ children }: LayoutProps){
     <>
       <div className="min-h-screen bg-slate-950 text-slate-200 antialiased overflow-x-hidden">
         {/* add justify-center to main if needed */}
-        <main className="min-h-screen flex flex-col justify-center items-center space-y-5"> 
+        <main className="flex flex-col justify-center items-center space-y-5 py-12 sm:py-16 lg:py-20"> 
           { children }
         </main>
       </div>
