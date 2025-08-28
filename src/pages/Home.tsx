d1be749ec14f7795773012f600c2ac4f9318d7f8
seanl26@uw.edu <seanl26@uw.edu>
2025-08-28 02:20:49 -0600

    completed functional home screen

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -51,7 +51,54 @@ function Home() {
   return (
     <Layout>
 
-      <section className="w-full max-w-xl mx-auto space-y-8">
+      {/* header */}
+      <header className="relative mx-auto w-11/12 sm:w-4/5 md:w-2/3 lg:w-1/2 xl:w-2/5 flex items-center justify-center space-x-8">
+
+        <img
+          src={me}
+          alt="Sean Liu Picture"
+          className="
+            rounded-full object-cover ring-2 h-20 w-20 sm:h-28 sm:w-28 md:h-32 md:w-32"
+        />
+        {/* Name + typewriter stack */}
+        <div className="flex flex-col items-center space-y-3">
+          <h1 className="font-semibold text-5xl md:text-7xl lg:text-8xl xl:text-8xl leading-none">
+            Sean Liu
+          </h1>
+          <span className="font-mono text-lg sm:text-xl md:text-2xl">
+            {/* typewriter animation will go here */}
+            {(phrases[i] ?? "").slice(0, count)}
+            <span aria-hidden className="ml-1 animate-pulse">▍</span>
+          </span>
+        </div>
+      </header>
+
+      <hr className="border-1 mx-auto w-11/12 sm:w-4/5 md:w-2/3 lg:w-1/2 xl:w-2/5" />
+
+      {/* main content for about goes here */}
+      {/* <section className="flex flex-col text-3xl font-semibold relative mx-auto w-11/12 sm:w-4/5 md:w-2/3 lg:w-1/2 xl:w-2/5 space-y-10"> */}
+      <section className="flex flex-col font-medium relative mx-auto w-11/12 sm:w-4/5 md:w-2/3 lg:w-1/2 xl:w-2/5 space-y-10 text-lg sm:text-xl md:text-2xl lg:text-3xl">
+        <p> hey, welcome to my page! 👋</p>
+        <p> 
+          i'm an undergraduate at the <a href="https://www.washington.edu" className="hover:text-blue-300 underline">university of washington</a> studying <a href="https://ischool.uw.edu/" className="hover:text-blue-300 underline">informatics</a>, concentrating in software development and data science. 
+        </p>
+        <p>i'm especially interested in AI/ML applications and data analysis, but love to learn a bit of everything! i am also passionate about sustainibility and accessibility.</p>
+        <p>feel free to reach out using the links below, i would love to get to know you!</p>
+        <p className="text-center">→ &nbsp; check out my <span className="font-bold hover:text-blue-300 underline"><Link to="/projects">projects</Link></span> &nbsp; ←</p>
+        
+      </section>
+
+      <hr className="border-1 mx-auto w-11/12 sm:w-4/5 md:w-2/3 lg:w-1/2 xl:w-2/5" />
+
+      <Footer />
+
+    </Layout>
+  )
+}
+
+export default Home
+
+      {/* <section className="w-full max-w-xl mx-auto space-y-8">
         
 
         <div className="flex items-center justify-center space-x-8">
@@ -81,10 +128,4 @@ function Home() {
 
         <Footer />
 
-      </section>
-
-    </Layout>
-  )
-}
-
-export default Home
+      </section> */}
