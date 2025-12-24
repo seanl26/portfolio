5d50cff99e63df75391aed5397c2fc71b17b8cab
seanl26 <seanl26@uw.edu>
2025-12-24 00:19:13 -0800

    added layout to pagenotfound page

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,8 +1,13 @@
+import Layout from "../components/PageLayout"
+
 function PageNotFound() {
+
     return (
-        <div className="text-center space-y-4">
-            <p>page not found. 404 error.</p>
-        </div>
+        <Layout>
+            <div className="text-center space-y-4">
+                <p>page not found. 404 error.</p>
+            </div>
+        </Layout>
     )
 }
 
